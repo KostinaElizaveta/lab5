a22a17199ea08445166657c0780448c017766f9e
KostinaElizaveta <lizaveta94@mail.ru>
2017-03-10 12:37:31 +0400

    Create foo.cpp

---
+++ foo.cpp
@@ -0,0 +1,11 @@
+#include <catch.hpp>
+unsigned int sum (unsigned int number1,unsigned int number2 )
+{
+return number1+number2;
+}
+TEST_CASE ("Sum", "[sum]")
+{
+REQUIRE( sum(1,2) == 3 );
+REQUIRE( sum(10,27) == 37 );
+REQUIRE( sum(17,16) == 33 );
+}
